542734d872edc35f46414355fb81f0b98e00416e
Glenn Hermans <glhermans@gmail.com>
2020-06-15 02:25:13 +0200

    Menu updated
    Added the menu link to catalog instead of a separated menu

--- packages/Webkul/Bulkupload/src/Config/admin-menu.php
+++ packages/Webkul/Bulkupload/src/Config/admin-menu.php
@@ -2,31 +2,25 @@
 
 return [
     [
-        'key'        => 'bulkupload',
-        'name'       => 'bulkupload::app.admin.bulk-upload.index',
-        'route'      => 'admin.bulk-upload.index',
-        'sort'       => 9,
-        'icon-class' => 'bulk-upload-icon',
-    ], [
-        'key'        => 'bulkupload.manage-bulk-upload',
+        'key'        => 'catalog.manage-bulk-upload',
         'name'       => 'bulkupload::app.admin.bulk-upload.manage-bulk-upload',
         'route'      => 'admin.dataflow-profile.index',
-        'sort'       => 1,
+        'sort'       => 4,
         'icon-class' => '',
     ], [
-        'key'        => 'bulkupload.manage-bulk-upload.data-flow-profile',
+        'key'        => 'catalog.manage-bulk-upload.data-flow-profile',
         'name'       => 'bulkupload::app.admin.bulk-upload.data-flow-profile.index',
         'route'      => 'admin.dataflow-profile.index',
         'sort'       => 1,
         'icon-class' => '',
     ], [
-        'key'        => 'bulkupload.manage-bulk-upload.upload-files',
+        'key'        => 'catalog.manage-bulk-upload.upload-files',
         'name'       => 'bulkupload::app.admin.bulk-upload.upload-files.index',
         'route'      => 'admin.bulk-upload.index',
         'sort'       => 2,
         'icon-class' => '',
     ], [
-        'key'        => 'bulkupload.manage-bulk-upload.run-profile',
+        'key'        => 'catalog.manage-bulk-upload.run-profile',
         'name'       => 'bulkupload::app.admin.bulk-upload.run-profile.index',
         'route'      => 'admin.run-profile.index',
         'sort'       => 3,
