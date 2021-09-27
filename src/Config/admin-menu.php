60faad7a2d2efa468f8d70fd25b6002bc14961bb
Vivek <viveksh047@webkul.com>
2021-09-27 20:49:30 +0530

    [Updated: admin-route.php, system.php, admin-menu.php file updated to make them compatible for saas subscription.]

--- src/Config/admin-menu.php
+++ src/Config/admin-menu.php
@@ -2,25 +2,25 @@
 
 return [
     [
-        'key'        => 'catalog.manage-bulk-upload',
+        'key'        => 'catalog.bulkupload',
         'name'       => 'bulkupload::app.admin.bulk-upload.manage-bulk-upload',
         'route'      => 'admin.dataflow-profile.index',
         'sort'       => 5,
         'icon-class' => '',
     ], [
-        'key'        => 'catalog.manage-bulk-upload.data-flow-profile',
+        'key'        => 'catalog.bulkupload.data-flow-profile',
         'name'       => 'bulkupload::app.admin.bulk-upload.data-flow-profile.index',
         'route'      => 'admin.dataflow-profile.index',
         'sort'       => 1,
         'icon-class' => '',
     ], [
-        'key'        => 'catalog.manage-bulk-upload.upload-files',
+        'key'        => 'catalog.bulkupload.upload-files',
         'name'       => 'bulkupload::app.admin.bulk-upload.upload-files.index',
         'route'      => 'admin.bulk-upload.index',
         'sort'       => 2,
         'icon-class' => '',
     ], [
-        'key'        => 'catalog.manage-bulk-upload.run-profile',
+        'key'        => 'catalog.bulkupload.run-profile',
         'name'       => 'bulkupload::app.admin.bulk-upload.run-profile.index',
         'route'      => 'admin.run-profile.index',
         'sort'       => 3,
